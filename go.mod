94708472048ddb1b6d006550b3e0beba4956dd63
Jeremy Kisner <kisner.jeremy@protonmail.com>
2023-11-14 21:09:40 -0500

    code clean up

--- go.mod
+++ go.mod
@@ -5,9 +5,7 @@ go 1.21.3
 require (
 	github.com/go-audio/audio v1.0.0
 	github.com/go-audio/wav v1.1.0
+	github.com/gorilla/mux v1.8.1
 )
 
-require (
-	github.com/go-audio/riff v1.0.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-)
+require github.com/go-audio/riff v1.0.0 // indirect
--- server.go
+++ server.go
@@ -2,29 +2,18 @@ package main
 
 import (
 	"net/http"
-	"sync"
 
 	"github.com/gorilla/mux"
 	"github.com/jeremyKisner/streaming-daemon/audioproducer"
 )
 
-const sampleRate = 44100
-
-var (
-	requestCounter int
-	mu             sync.Mutex
-)
-
 func main() {
 	r := mux.NewRouter()
-
-	// r.HandleFunc("/")
-	r.HandleFunc("/", handleStream)
-
+	r.HandleFunc("/beepstream", handleBeepStream)
 	http.ListenAndServe(":8080", r)
 }
 
-func handleStream(w http.ResponseWriter, r *http.Request) {
+func handleBeepStream(w http.ResponseWriter, r *http.Request) {
 	Producer := audioproducer.Producer{}
 	Producer.StreamRandomBeeps(w, r)
 }
